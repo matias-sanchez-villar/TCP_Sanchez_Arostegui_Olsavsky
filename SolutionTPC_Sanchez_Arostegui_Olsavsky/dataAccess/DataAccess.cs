cbfcceda1c7a27e9f7fc47aa2f1dbfce6cff8242
prodriguezolsavsky <81335679+prodriguezolsavsky@users.noreply.github.com>
2021-06-21 20:06:32 -0300

    Creacion Clases DataAccess y Persona

---
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/dataAccess/DataAccess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace dataAccess
+{
+    class DataAccess
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        string cadenaConexion = "data source = (local)\\SQLEXPRESS; initial catalog =Sanchez_Arostegui_Olsavsky_DB; integrated security = true;";
+
+        public DataAccess()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            lector = comando.ExecuteReader();
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        internal void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+    }
+}
---
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/dominio/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string DNI { get; set; }
+        public char Genero { get; set; }
+        public string Domicilio { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+
+
+
+
+    }
+}
