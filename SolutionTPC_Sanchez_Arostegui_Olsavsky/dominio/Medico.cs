99f0a7ad88199e54f7bda43dbcfd19aa616f7c77
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2021-06-23 03:06:45 -0300

    Ahora si mucho mejor

--- SolutionTPC_Sanchez_Arostegui_Olsavsky/dominio/Medico.cs
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/dominio/Medico.cs
@@ -13,51 +13,5 @@ namespace dominio
         public string Matricula { get; set; }
         public string Especialidad { get; set; }
 
-        public List<Medico> Listar()
-        {
-            List<Medico> Lista = new List<Medico>();
-            DataAcces Datos = new DataAcces();
-
-            try
-            {
-                string Consulta = "22";
-
-                Datos.setearConsulta(Consulta);
-
-                Datos.ejecutarLectura();
-
-                while (Datos.Lector.Read())
-                {
-                    Medico medico = new Medico();
-
-                    medico.ID = (int)Datos.Lector["ID"];
-                    medico.DNI = (string)Datos.Lector["DNI"];
-                    medico.Nombre = (string)Datos.Lector["Nombre"];
-                    medico.Apellido = (string)Datos.Lector["Apellido"];
-                    medico.Matricula = (string)Datos.Lector["Matricula"];
-                    medico.Especialidad = (string)Datos.Lector["Especialidad"];
-                    medico.Domicilio = (string)Datos.Lector["Domicilio"];
-                    medico.Email = (string)Datos.Lector["Email"];
-                    medico.Constrasena = (string)Datos.Lector["Pass"];
-                    medico.Especialidad = (string)Datos.Lector["Especialidad"];
-                    medico.Celular = (string)Datos.Lector["Telefono"];
-                    medico.Estado = (bool)Datos.Lector["Estado"];
-                    medico.FechaNacimiento = (DateTime)Datos.Lector["FechaNacimiento"]; 
-                    medico.Genero = (char)Datos.Lector["Genero"];
-
-                    Lista.Add(medico);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                Datos.cerrarConexion();
-            }
-
-            return Lista;
-        }
     }
 }
---
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/negocio/N_Medico.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using dataAccess;
+
+namespace negocio
+{
+    public class N_Medico
+    {
+        public Medico Medico { get; set; }
+        public List<Medico> Lista { get; set; }
+        public DataAcces Datos { get; set; }
+
+        public N_Medico()
+        {
+            Datos = new DataAcces();
+        }
+
+        public List<Medico> Listar()
+        {
+            Lista = new List<Medico>();
+
+            try
+            {
+                string Consulta = "22";
+
+                Datos.setearConsulta(Consulta);
+
+                Datos.ejecutarLectura();
+
+                while (Datos.Lector.Read())
+                {
+                    Medico medico = new Medico();
+
+                    medico.ID = (int)Datos.Lector["ID"];
+                    medico.DNI = (string)Datos.Lector["DNI"];
+                    medico.Nombre = (string)Datos.Lector["Nombre"];
+                    medico.Apellido = (string)Datos.Lector["Apellido"];
+                    medico.Matricula = (string)Datos.Lector["Matricula"];
+                    medico.Especialidad = (string)Datos.Lector["Especialidad"];
+                    medico.Domicilio = (string)Datos.Lector["Domicilio"];
+                    medico.Email = (string)Datos.Lector["Email"];
+                    medico.Constrasena = (string)Datos.Lector["Pass"];
+                    medico.Especialidad = (string)Datos.Lector["Especialidad"];
+                    medico.Celular = (string)Datos.Lector["Telefono"];
+                    medico.Estado = (bool)Datos.Lector["Estado"];
+                    medico.FechaNacimiento = (DateTime)Datos.Lector["FechaNacimiento"];
+                    medico.Genero = (char)Datos.Lector["Genero"];
+
+                    Lista.Add(medico);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Datos.cerrarConexion();
+            }
+        }
+    }
+}
