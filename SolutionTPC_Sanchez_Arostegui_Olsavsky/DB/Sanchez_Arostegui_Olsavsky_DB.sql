4bb8fb44043c38619afe6a686c227d16a5d1cf2d
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2021-06-29 01:16:29 -0300

    Script Listos

--- SolutionTPC_Sanchez_Arostegui_Olsavsky/DB/Sanchez_Arostegui_Olsavsky_DB.sql
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/DB/Sanchez_Arostegui_Olsavsky_DB.sql
@@ -1,8 +1,8 @@
 --USE MASTER
 --GO
 --GO
-DROP DATABASE Sanchez_Arostegui_Olsavsky_DB
-GO
+--DROP DATABASE Sanchez_Arostegui_Olsavsky_DB
+--GO
 --GO
 
 CREATE DATABASE Mediturnos
@@ -80,23 +80,60 @@ CREATE TABLE CALENDARIO(
 
 Set dateformat 'DMY'
 
-/* PACIENTES */
---INSERT INTO Pacientes([ID], [Apellido], [Nombre], [FechaNacimiento],[Domicilio],[EMail], [Celular], [Genero], [NroAfiliado], [ObraSocial], [Estado]) VALUES(1,'Maldonado','Alejandro','24/03/1994', 'Av. Cordoba 2345', 'aleejo@lorenita.com', '118765555', 'M', '123444', 'Swiss Medical', 1),(2,'Guevara','Marcelo','24/05/1984', 'Av. Maipu 2355', 'marcelo@gachate.com', '118556677', 'M', '32444', 'OSDE', 1),(3,'Armandinho','Favelo','09/01/1974', 'Av. Brasil 4345', 'clarao@llalaa.com', '118737895', 'M', '54321', 'DOSUBA', 1),(4,'Marcowick', 'Maria Elena','14/09/1991', 'Av. Las Perlas 6345', 'marialespe@tequiero.com', '11845325', 'F', '87234', 'GALENO', 1),(5,'Perez','Carmela','29/05/1955', 'Av. Santa fe 5435', 'carmelita@alguna.com', '1156783322', 'F', '998742', 'Swiss Medical', 1),(6,'Maldonado','Alejandro','24/03/1994', 'Av. Cordoba 2345', 'aleejo@lamorenita.com', '118765555', 'M', '123444', 'Swiss Medical', 1),(7,'Marengo','Johny','12/03/1995', 'Av. Nidea 6645', '', '117886545', 'M', '5434441', 'Encubreti', 1),(8,'Rolando','Alberto Fasito','30/03/1964', 'Av. Siempreviva 2222', 'alegre@siempre.com', '112223334', 'M', '776655', 'Swiss Medical', 1),(9,'Gomez','Gabriela','11/03/1944', 'Av. Cordoba 1111', 'gaby_99@alexandria.com', '114433223', 'F', '322133', 'DOSUBA', 1),(10,'Ramirez','Gerarda Romula','22/02/1997', 'Rotarix 1111', 'Gerard@general.com', '112398675', 'O', '454343', 'OSDE', 1);
-/* MEDICOS */
---INSERT INTO Medicos([ID],[Nombre],[Apellido],[FechaNacimiento],[Domicilio],[EMail], [Celular], [Genero], [NroMatricula], [Especialidad], [Estado]) VALUES(1,'Quiroga','Horacio','14/03/1954', 'Av. Cordoba 1111', 'Hor_q@libros.com.ar', '1129098765', 'M', '12345', 'Clinico', 1),(2,'Funes','Alejandro','12/03/1964', 'Av. Coronel Diaz 2345', 'funes@elmemorioso.com', '112365478', 'M', '222222', 'Psiquiatria', 1),(3,'Maldonado','Alejandro','24/03/1994', 'Av. Radiografo 2345', 'tecuro@tetrato.com', '1144337898', 'M', '444444', 'Traumatologia', 1),(4,'De Arco','Juana','01/01/1966', 'Av. Las Batallas 6777', 'juanita@tecacha.com', '11345678', 'F', '55555', 'Cardiologia', 1),(5,'Mercolario','Marian','24/03/1955', 'Loquesea 1322', 'marinero@lala.com', '1123433322', 'M', '667767', 'Urologia', 1),(6,'Rucci','Paola','24/07/1978', 'Av. Sueños 2222', 'lamdeci@salvadora.com', '1122987456', 'F', '888776', 'Neurologia', 1);
-/* TURNOS */
---INSERT INTO Turnos([FechaHora],[IDMedico],[IDPaciente], [Estado]) VALUES('01/07/2020 07:40',1,1, 1),('01/07/2021 17:40',2,2, 1),('01/05/2020 18:30',3,3, 1),('04/04/2020 10:40',4,4, 1),('01/01/2021 11:20',1,1, 1),('01/11/2021 09:40',1,5, 1),('11/09/2021 16:30',2,7, 1),('01/08/2021 13:20',3,8, 1),('22/09/2022 13:40',4,10, 1),('19/11/2021 11:40',5,9, 1);
+--18 Usuarios
+insert into Usuarios (Email, Contasena, Estado) VALUES
+('Pepe@gmail.com', '1234', 1), 
+('Pato@hotmail.com', '1234', 1),
+('Paisa@gmail.ar', '1234', 1),
+('Doro@hotmail.com', '1234', 1), 
+('Pablo@gmail.com', '1234', 1), 
+('Pablo22@gmail.com', '1234', 1), 
+('Toto@gmail.com', '1234', 1), 
+('Elepa_roro@gmail.com', '1234', 1), 
+('Medic_bom@gmail.com', '1234', 1),
+('gonzagomez@yahoo.com', '1234', 1),
+('pipi99@yahoo.com', '1234', 1),
+('marispert@gmail.com', '1234', 1),
+('ejemplo@gmail.com', '1234', 1),
+('pedro_escov@gmail.com', '1234', 1),
+('delfi_per@yahoo.com.ar', '1234', 1),
+('magali1996@gmail.com', '1234', 1),
+('caroperez12@gmail.com', '1234', 1),
+('DanteClick@gmail.com', '1234', 1),
+('ElChanta@gmail.com', '1234', 1),
+('pablobonf@yahoo.com.ar', '1234', 1)
 
-
-//9 Usuarios
-insert into Usuarios (Email, Contasena, Estado) VALUES ('Pepe@gmail.com', '1234', 1) , ('Pato@hotmail.com', '1234', 1), ('Paisa@gmail.ar', '1234', 1), ('Doro@hotmail.com', '1234', 1), ('Pablo@gmail.com', '1234', 1), ('Pablo22@gmail.com', '1234', 1), ('Toto@gmail.com', '1234', 1), ('Elepa_roro@gmail.com', '1234', 1), ('Medic_bom@gmail.com', '1234', 1)
-
-//8 Especialidades
+--8 Especialidades
 insert into Especialidades (Especialidad) values ('Dermatología'), ('Oftalmología'), ('Traumatología'), ('Urología'), ('Oftalmología'), ('Otorrinolaringología'), ('Ginecología'), ('CARDIOLOGIA')
 
-//5 medicos
-insert into Medicos(Nombre, Apellido, Domicilio, Celular, FechaNacimiento, Genero, Matricula, IDUsuario, IDEspecialidad) values ('Pepe','Luis','Falsa 123','1234560','24/03/1994','M','1234', 1, 1), ('Pato','Goye','Estol 321','2234542','20/05/1984','M','2234', 2, 2), ('Matias','Paisa','Blegrano 456','8954624','09/07/1996','M','7894', 3, 1),('Elsa','Alfano','Argent 753','7893214','10/10/2000','M','6666', 9, 5),('Coco','Rococo','Roca 5503','2312655','15/08/2005','M','4466', 8, 5)
+--10 medicos
+insert into Medicos(Nombre, Apellido, Domicilio, Celular, FechaNacimiento, Genero, Matricula, IDUsuario, IDEspecialidad) values
+('Pepe','Luis','Falsa 123','1234560','19980223','M','1234', 1, 1), 
+('Pato','Goye','Brown 321','2234542','19900203','M','2234', 2, 2),
+('Matias','Paisa','Blegrano 456','8954624','19940205','M','7894', 3, 1),
+('Isidoro','Arostegui','Medina 1775','7363214','19980729','M','4321', 4, 6),
+('Pablo','Olsavsky','estomba 2594','7793278','19970502','M','2345', 5, 8),
+('Pablo','Sorrivas','gorriti 53','9893295','19940815','M','7894', 6, 3),
+('Tota','Garcia','San Martin 4562','4594514','19801226','F','9874', 7, 5),
+('Elsa','mazzochi','Saavedra 894','7594214','19880317','F','4561', 8, 7),
+('Daina','Alfano','Castelli 1539','4563321','19900101','F','6541', 9, 7),
+('Gonzalo','gomez','Roca 32312','2312555','19961118','M','3691', 10, 7)
 
 
+--6 Obras sociales
+insert into ObrasSociales(ObraSocial) values ('DIVA'), ('OSDE'), ('MEDAFI'), ('GALENO'), ('MEDICUS'), ('IOMA')
+
+
+--10 pacientes
+insert into Pacientes(Nombre, Apellido, Domicilio, Celular, FechaNacimiento, Genero, NroAfiliado, IDUsuario, IDObraSocial) values
+('Maria','Gonzales','Rivadavia 1233','1234560','19961118','F','1234', 11, 1), 
+('Mariel','Espert','Güemes 1223','2312555','19980223','F','7412', 12, 2), 
+('Muestra','Ejemplo','Brown 1123','4563321','19940205','F','8523', 13, 5), 
+('Delfina','Rapani','Alberdi 453','4594514','19801225','F','1234', 14, 4), 
+('Magali','Ginzo','Rosas 1208','9893295','19990703','F','9632', 15, 1), 
+('Carolina','Perez','Artigas 1024','1130060','19910420','F','8945', 16, 6), 
+('Dante','Abecia','Bolivar 3015','1439630','19981117','M','6541', 17, 1), 
+('Chanta','Pirola','San Martin 645','7414560','19980729','M','9874', 18, 3), 
+('Pablo','Bonfilio','Belgrano 456','1334852','19940520','M','9512', 19, 1)
 
 
