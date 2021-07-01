1206b4e2ed42f97aca3e987341093c4894879020
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2021-07-01 04:08:02 -0300

    Clase para Lista Especialidad

--- SolutionTPC_Sanchez_Arostegui_Olsavsky/dominio/Especialidad.cs
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/dominio/Especialidad.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using dataAccess;
 
 namespace dominio
 {
-    public class Medico : Persona
+    public class Especialidad
     {
         public int ID { get; set; }
-        public Especialidad especialidad { get; set; }
-        public string Matricula { get; set; }
+        public string Nombre { get; set; }
     }
 }
---
+++ SolutionTPC_Sanchez_Arostegui_Olsavsky/negocio/N_Especialidad.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using dataAccess;
+
+
+namespace negocio
+{
+    public class N_Especialidad
+    {
+        List<Especialidad> lista;
+        DataAcces datos;
+
+        public List<Especialidad> listar()
+        {
+            datos = new DataAcces();
+            lista = new List<Especialidad>();
+
+            try
+            {
+                datos.setearConsulta(" select ID, ObraSocial from ObrasSociales ");
+
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Especialidad aux = new Especialidad();
+
+                    aux.ID = (int)datos.Lector["ID"];
+                    aux.Nombre = (string)datos.Lector["ObraSocial"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
